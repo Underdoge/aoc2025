3d9456f1775ea0acf801b15a9a51691f733d373f
Eduardo Chapa <eduardo.chapa@gmail.com>
2025-12-18 16:56:43 -0600

    simplify

--- day7/day7_part2.py
+++ day7/day7_part2.py
@@ -21,18 +21,15 @@ def read_manifold(data: str) -> list:
 
 def paths(manifold: list, y: int, x: int, path: dict) -> int:
     while y + 1 < len(manifold):
-        if manifold[y][x] == '|':
-            if y + 1 < len(manifold):
-                y += 1
+        if manifold[y][x] == '|' and y + 1 < len(manifold):
+            y += 1
         elif manifold[y][x] == "^":
-            count = 0
             if str([y,x]) not in path:
-                count += paths(manifold, y+1, x-1, path)
+                count = paths(manifold, y+1, x-1, path)
                 count += paths(manifold, y+1, x+1, path)
                 path[str([y,x])] = count
-                return count
             else:
-                count += path[str([y,x])]
+                count = path[str([y,x])]
             return count
     return 1
 
