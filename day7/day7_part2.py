b6586c3e08a00e8ae99ad01f2ac86945d72d5610
Eduardo Chapa <eduardo.chapa@gmail.com>
2025-12-18 16:43:24 -0600

    day7

--- day7/day7_part2.py
+++ day7/day7_part2.py
@@ -1,46 +1,51 @@
 import sys
 
-
-def show_path(manifold):
-    print("".join(manifold[len(manifold)-1]))
-
 def read_manifold(data: str) -> list:
     manifold = []
+    splits = 0
     with open(data, 'r') as file:
         for line in file:
             manifold.append(list(line.strip()))
+    for y in range(len(manifold)):
+        for x in range(len(manifold[0])):
+            if manifold[y][x] == "S":
+                manifold[y+1][x] = "|"
+            elif manifold[y][x] == "^" and manifold[y-1][x] == '|':
+                manifold[y+1][x-1] = "|"
+                manifold[y+1][x+1] = "|"
+                splits += 1
+            elif manifold[y][x] == '|':
+                if y+1 < len(manifold) and manifold[y+1][x] == '.':
+                    manifold[y+1][x] = '|'
     return manifold
 
-def paths(manifold: list, y: int, x: int) -> int:
-    while y + 1 < len(manifold): 
-        if manifold[y][x] == "S":
-            manifold[y+1][x] = "|"
-            y += 1
-        elif manifold[y][x] == "^":
-            manifold[y+1][x-1] = "|"
-            left = paths(manifold, y+1, x-1)
-            manifold[y+1][x+1] = "|"
-            right = paths(manifold, y+1, x+1)
-            return left + right
-        elif manifold[y][x] == '|':
+def paths(manifold: list, y: int, x: int, path: dict) -> int:
+    while y + 1 < len(manifold):
+        if manifold[y][x] == '|':
             if y + 1 < len(manifold):
-                if manifold[y+1][x] == '.':
-                    manifold[y+1][x] = '|'
                 y += 1
-    show_path(manifold)
+        elif manifold[y][x] == "^":
+            count = 0
+            if str([y,x]) not in path:
+                count += paths(manifold, y+1, x-1, path)
+                count += paths(manifold, y+1, x+1, path)
+                path[str([y,x])] = count
+                return count
+            else:
+                count += path[str([y,x])]
+            return count
     return 1
 
 def count_paths(data: str) -> int:
     manifold = read_manifold(data)
 
-    for line in manifold:
-        print("".join(line))
+    # for line in manifold:
+    #     print("".join(line))
     
     for y in range(len(manifold)):
         for x in range(len(manifold[0])):
             if manifold[y][x] == 'S':
-                path_count = paths(manifold, y, x)
-                print("paths:",path_count)
+                path_count = paths(manifold, y+1, x, {})
                 break
     
     return path_count
